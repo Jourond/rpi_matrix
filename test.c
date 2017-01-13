d17941e3180b1c2c3bc08cfc050a40f2926475f9
Jourond <JourdonSorg@gmail.com>
2017-01-13 10:05:09 -0600

    changing refresh rate

--- test.c
+++ test.c
@@ -13,11 +13,11 @@ int main(void)
 	static uint8_t i = 1;                                                       
 
 	wiringPiSetup();									// initialize wiringPi 
-	wiringPiSPISetup(0,500000);							// initialize SPI  information, 0 is channel 0, 500000 is clock rate.
+	wiringPiSPISetup(0,800000);							// initialize SPI  information, 0 is channel 0, 500000 is clock rate.
 	while(1){
 		int j;
 		int x = 2;
-		static uint8_t heart[8] = {0x8F, 0x81, 0x81, 0x81, 0x8F, 0x80, 0x80, 0x80};					// this is a array of heart
+		static uint8_t heart[8] = {0xF8, 0x81, 0x81, 0x81, 0x8F, 0x80, 0x80, 0x80};					// this is a array of heart
 		for ( j=0;j<8;j++)
 		{
 			data[0] = ~heart[j];
